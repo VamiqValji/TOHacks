47fcfd45527748b85eb94a3411f5d9bbb0daa239
Vamiq <vamiq.valji@gmail.com>
2021-05-08 18:27:04 -0400

    added user reducer and action

--- client/src/pages/Forms/Forms.tsx
+++ client/src/pages/Forms/Forms.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import { Box } from '@chakra-ui/layout';
+import { Flex, Heading } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 interface FormsProps {}
 
-const Forms: React.FC<FormsProps> = ({}) => {
+const Forms: React.FC<FormsProps> = () => {
+
+    const fetchForms = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:3001/forms/view");
+            console.log(data)
+        } catch(e) { console.log(e); }
+    }
+
+    useEffect(() => {
+        fetchForms();
+    }, [])
+
     return (
     <>
-        
+        <Flex w={"50vw"} m={"0 auto"} mt={4} >
+            <Box className="leftContainer" width={"50%"}>
+                <Heading>Your Forms</Heading>
+            </Box>
+            <Box className="rightContainer" width={"50%"}>
+                <Heading>Your Forms' Responses</Heading>
+            </Box>
+        </Flex>
     </>);
 }
 
---
+++ client/src/redux/actions/userAction.ts
@@ -0,0 +1,11 @@
+export const setUserData = () => {
+    return {
+      type: "SET_USER_DATA",
+    };
+}; 
+
+export const clearUserData = () => {
+    return {
+      type: "CLEAR_USER_DATA",
+    };
+}; 
