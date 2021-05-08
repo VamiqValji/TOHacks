ec355dad7421132b4f18bcad699d571c9ad9263b
Vamiq <vamiq.valji@gmail.com>
2021-05-08 13:16:36 -0400

    chakra setup // only works with firefox

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import * as React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./chakra/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
