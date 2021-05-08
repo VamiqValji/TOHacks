20cf48dd7956f6519ce71bf7713ed7ebea9d31e1
Vamiq <vamiq.valji@gmail.com>
2021-05-08 14:11:43 -0400

    added interface for routes

---
+++ src/ts/interface.ts
@@ -0,0 +1,31 @@
+export interface response {
+    question: string,
+    response: string,
+}
+
+export interface formResponse {
+    userWhoResponded: string,
+    date: string,
+    questionsAndResponses: response[],
+}
+
+export interface question {
+    id: string,
+    question: string,
+    description?: string,
+    inputField: string,
+}
+
+export interface form {
+    title: string,
+    description: string,
+    questions: question[],
+    responses: formResponse[]
+}
+
+export interface usersInterface {
+    userId: string,
+    name: string,
+    password: string,
+    forms: form[]
+}
