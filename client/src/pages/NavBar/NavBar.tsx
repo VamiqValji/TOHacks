ba288398ebdb4bf247f5227e3541e5827929375b
Vamiq <vamiq.valji@gmail.com>
2021-05-09 01:29:33 -0400

    fix nav bar links

--- client/src/pages/NavBar/NavBar.tsx
+++ client/src/pages/NavBar/NavBar.tsx
@@ -3,20 +3,8 @@ import ReactNode from 'react';
 import {
   Box,
   Flex,
-  Avatar,
-  HStack,
-  Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
 } from '@chakra-ui/react';
+import { Link } from "react-router-dom";
 import {HamburgerIcon,EditIcon} from '@chakra-ui/icons';
 
 interface NavBarProps {}
