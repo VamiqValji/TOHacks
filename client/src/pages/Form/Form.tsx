b83ea8b998d7e2c2b6d8e1a3da2e529e302ce35c
Vamiq <vamiq.valji@gmail.com>
2021-05-09 06:04:58 -0400

    basic logic for form.tsx done

--- client/src/pages/Form/Form.tsx
+++ client/src/pages/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Spinner, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Input, Spinner, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
@@ -10,6 +10,7 @@ interface FormProps {}
 const Form: React.FC<FormProps> = () => {
 
     const [form, setForm] = useState<form | null>(null);
+    const [buttonIsLoading, setButtonIsLoading] = useState<boolean>(false);
 
     const { id }:{id: string} = useParams();
 
@@ -37,11 +38,40 @@ const Form: React.FC<FormProps> = () => {
             </>);
     };
 
+    const renderQuestions = () => {
+        return form.questions.map((question) => {
+            return (
+                <>
+                    <Box className="form_question" bg={"blackAlpha.500"} p={4} borderRadius={4} mt={4}>
+                        <Text>{question.question}</Text>
+                        <Input mt="2" mb="2" type="text" variant="filled" placeholder={question.description} required />
+                        {/* <Input mb="2" type="text" value={id} style={{display:"none"}} required /> */}
+                    </Box>
+                </>
+            );     
+        });
+    };
+
+    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setButtonIsLoading(true);
+        // api calls
+        setButtonIsLoading(false);
+    };
+
     return (
     <>
-        <Box className="container" p={4} borderRadius={4} mt={4}>
-            <Heading></Heading>
-            <Text></Text>           
+        <Box className="container" m={"0 auto"} bg={"blackAlpha.500"} p={4} borderRadius={4} mt={4}>
+            <Heading size={"lg"}>{/*<b>Title:</b> */}{form.title}</Heading>
+            <Text size={"md"}>{/*<b>Description:</b> */}{form.description}</Text>
+            <form onSubmit={(e) => handleFormSubmit(e)}>
+                {renderQuestions()}
+                <Flex justifyContent="center">
+                    <Button size="lg" isLoading={buttonIsLoading} loadingText={"Submitting"} value={"Submit Form"} cursor="pointer" mt="4" type="submit" variant="solid" colorScheme="brand">
+                        Submit Form
+                    </Button>
+                </Flex>
+            </form>
         </Box>
     </>);
 }
