bd55e70db7234b5b26cb28c37a2ab0fa5319b706
Vamiq <vamiq.valji@gmail.com>
2021-05-09 05:47:51 -0400

    changed Forms.tsx state from questions to hold forms

--- client/src/pages/Form/Form.tsx
+++ client/src/pages/Form/Form.tsx
@@ -1,15 +1,15 @@
-import { Heading } from '@chakra-ui/react';
+import { Box, Heading, Spinner, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import MiddleOfScreenText from '../../components/MiddleOfScreenText';
-import { question } from '../../ts/interface/userInterface';
+import { form, question } from '../../ts/interface/userInterface';
 
 interface FormProps {}
 
 const Form: React.FC<FormProps> = () => {
 
-    const [questions, setQuestions] = useState<question[] | null>(null);
+    const [form, setForm] = useState<form | null>(null);
 
     const { id }:{id: string} = useParams();
 
@@ -18,7 +18,8 @@ const Form: React.FC<FormProps> = () => {
             const { data } = await axios.post("http://localhost:3001/forms/viewOne", {
                 formId: id,
             });
-            setQuestions(data.data.questions);
+            setForm(data.data);
+            console.log(data.data);
         } catch (e) { console.log(e); }
     };
 
@@ -26,20 +27,22 @@ const Form: React.FC<FormProps> = () => {
         fetchData();
     }, []);
 
-    if (questions === null || questions.length === 0) {
-        // ^ both conditions give off same side effect
-        // as questions length cant be 0
+    if (form === null) {
         return (
             <>
                 <MiddleOfScreenText>
-                    <Heading>Loading or error...</Heading>
+                    {/* <Heading>Loading or error...</Heading> */}
+                    <Spinner size="xl"></Spinner>
                 </MiddleOfScreenText>
             </>);
     };
 
     return (
     <>
-        
+        <Box className="container" p={4} borderRadius={4} mt={4}>
+            <Heading></Heading>
+            <Text></Text>           
+        </Box>
     </>);
 }
 
